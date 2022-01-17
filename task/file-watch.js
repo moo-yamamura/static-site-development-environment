08567c5dd40a2bf4329d270dac14289659f12176
karabiner-moo.yamamura <moo.yamamura@karabiner.tech>
2022-01-17 21:19:15 +0900

    初期コミット

---
+++ task/file-watch.js
@@ -0,0 +1,34 @@
+import gulp from 'gulp';
+import { path } from './path.js'; // ファイルパス
+import watch from 'gulp-watch'; // ファイル監視
+import sassCompile from './sass-compile.js'; // sassコンパイル
+import javascriptLint from './javascript-lint.js'; // javascriptコードチェック
+import javascriptCompile from './javascript-compile.js'; // javascriptコンパイル
+import javascriptBundle from './javascript-bundle.js'; // javascriptバンドル
+import ejsCompile from './ejs-compile.js'; // ejsコンパイル
+import staticDest from './static-dest.js'; // 静的ファイル出力（画像圧縮）
+
+//
+// task
+//////////////////////////////////////////////////////////////////////
+const fileWatch = (done) => {
+  watch(path.scss.src,gulp.series(sassCompile));
+
+  watch(path.js.src,
+    gulp.series(
+      gulp.parallel(
+        javascriptLint,
+        javascriptCompile,
+        javascriptBundle
+      )
+    )
+  );
+
+  watch(path.ejs.src,gulp.series(ejsCompile));
+  
+  watch(path.file.static, gulp.series(staticDest));
+
+  done();
+};
+
+export default fileWatch;
